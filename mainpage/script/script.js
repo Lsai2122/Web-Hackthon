f171885987ee4540a96fab1546e7f2c03d066abc
Mouryagna <mouryagnabaindla@gmail.com>
2025-02-20 11:21:04 +0530

    20-2-2025

--- mainpage/script/script.js
+++ mainpage/script/script.js
@@ -1,3 +1,12 @@
 document.querySelector('.about').addEventListener('click',() => {
-    const about=`${<button>Act(PP)</button>}`
+    const about=`
+    <button>Act(PP)</button>
+    <button>Statute</button>
+    <button>Institue Profile</button>
+    <button>Facilities</button>
+    <button>Annual Report</button>
+    <button>MoU</button>
+    `;
+    document.querySelector('.about-pop').innerHTML=about;
+    document.querySelector('.about-pop').classList.add('about-style');
 })
