dd4d88d7b8c81b419aa2f89db89916713a6724b0
Mouryagna <mouryagnabaindla@gmail.com>
2025-02-22 14:23:49 +0530

    21-02-2025

--- mainpage/script/script.js
+++ mainpage/script/script.js
@@ -1,11 +1,11 @@
 document.querySelector('.about').addEventListener('click',() => {
     const about=`
-    <button>Act(PP)</button>
-    <button>Statute</button>
-    <button>Institue Profile</button>
-    <button>Facilities</button>
-    <button>Annual Report</button>
-    <button>MoU</button>
+    <button class="about-button">Act(PP)</button>
+    <button class="about-button">Statute</button>
+    <button class="about-button">Institue Profile</button>
+    <button class="about-button">Facilities</button>
+    <button class="about-button">Annual Report</button>
+    <button class="about-button">MoU</button>
     `;
     document.querySelector('.about-pop').innerHTML=about;
     document.querySelector('.about-pop').classList.add('about-style');
