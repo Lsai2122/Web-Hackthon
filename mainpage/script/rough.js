2fcd37330f6ab0afccdc20febbdbd572938af08a
Lsai2122 <leelasaineralla@gmail.com>
2025-02-23 23:25:45 +0530

    clarity

--- mainpage/script/rough.js
+++ mainpage/script/rough.js
@@ -1,9 +1,11 @@
-list = ["../images/photo-campus/IIIT1.jpg","../images/photo-campus/IIIT2.jpg","../images/photo-campus/IIIT3.jpg","../images/photo-campus/IIIT4.jpg","../images/photo-campus/IIIT5.jpg","../images/photo-campus/IIIT6.jpg"]
-setInterval(()=>
-{
-    for(let i=0;i<100000;i++){
-        document.querySelector(".a").setAttribute("src",list[i%3])
-        document.querySelector(".b").setAttribute("src",list[i+1])
-        document.querySelector(".c").setAttribute("src",list[i+2])
-    }
-})
+list = ["../images/photo-campus/IIIT1.jpg", "../images/photo-campus/IIIT12.jpg", "../images/photo-campus/IIIT13.jpg", "../images/photo-campus/IIIT14.jpg", "../images/photo-campus/IIIT15.jpg", "../images/photo-campus/IIIT16.jpg"]
+i=0;
+setInterval(() => {
+    setTimeout(()=>
+    {
+        document.querySelector(".a").setAttribute("src", list[i % 6])
+    },10)
+    document.querySelector(".b").setAttribute("src", list[(i + 1 ) % 6])
+    i++;
+}, 3000)
+console.log(4%6)
