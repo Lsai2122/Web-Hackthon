f6adf014e6d552f940e213e19029e0a397051a3d
Lsai2122 <leelasaineralla@gmail.com>
2025-02-24 08:49:38 +0530

    madded js

--- mainpage/script/mainpage.js
+++ mainpage/script/mainpage.js
@@ -68,7 +68,7 @@ document.querySelector('.department').addEventListener('click', () => {
 
 document.querySelector('.students').addEventListener('click', () => {
     const students = `
-    <button class="students-button">Students Achievements</button>
+    <button class="students-button" onclick="window.location.href = window.location.origin + '/students/students-ach.html'">Students Achievements</button>
     <button class="students-button">Activities</button>
     <button class="students-button">Scholarships</button>
     <button class="students-button">Student Counseller</button>
